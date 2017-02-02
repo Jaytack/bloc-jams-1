3187f3391abdd004f64b4a67c77ed3303d5331a2
Timothy Schmidt <tim@Timothys-MacBook.local>
2017-02-01 20:18:54 -0500

    Assignment 8 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,22 @@
 var animatePoints = function() {
                 
     var points = document.getElementsByClassName('point');
+    
+    var revealPoint = function(index) {
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+    for(var i=0; i < points.length; i++) {
+        revealPoint(i);
+    }
+};
+
+/*below is the original function from Checkpoint 8*/
+/*var animatePoints = function() {
+                
+    var points = document.getElementsByClassName('point');
                 
     var revealFirstPoint = function() {
         points[0].style.opacity = 1;
@@ -27,4 +43,4 @@ var animatePoints = function() {
     revealSecondPoint();
     revealThirdPoint();
 
-};
+};*/
